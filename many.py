fded6679fc53f4a3f6c5d542a126ce3b6ec33930
Manue-droi <manusarf16@gmail.com>
2023-08-21 22:17:47 -0500

    c

--- many.py
+++ many.py
@@ -1,42 +1,50 @@
 class Asiento:
-
-
+    
     def __init__(self, color, precio, registro):
         self.color = color
         self.precio = precio
         self.registro = registro
-
-    def cambiarColor(self, nuevoColor, color):
-        if (nuevoColor == "rojo" or nuevoColor =="verde" or nuevoColor == "amarillo" or nuevoColor =="negro" or nuevoColor == "blanco"):
-            self.nuevoColor = str(color)
-
-
+    
+    def cambiarColor(self, nuevoColor):
+        if (nuevoColor == "rojo" or nuevoColor == "verde" or nuevoColor == "amarillo" or nuevoColor == "negro" or nuevoColor == 'blanco'):
+            self.color = str(nuevoColor)
+            
 class Motor:
+
     def __init__(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros= numeroCilindros
+        self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
-
+    
     def cambiarRegistro(self, nuevoRegistro):
         self.registro = int(nuevoRegistro)
-
+    
     def asignarTipo(self, nuevoTipo):
-        if nuevoTipo == "electrico" or nuevoTipo =="gasolina":
-            self.tipo = str(nuevoTipo)   
+        if (nuevoTipo == "electrico" or nuevoTipo == "gasolina"):
+            self.tipo = str(nuevoTipo)
 
 class Auto:
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
-        self.preciof = precio
+        self.precio = precio
         self.Asiento = asientos
-        self.marca= marca
+        self.marca = marca
         self.Motor = motor
         self.registro = registro
-        self.cantidadCreados = cantidadCreados
+        
 
-    def cantidadAsientos(self, asientos ):
-        asientos = asientos
+    def cantidadAsientos(self):
+        count = 0
     
+        for i in self.Asiento:
+            if i != None:
+                count =+ 1
+            
+        return count
+
 
 
  
